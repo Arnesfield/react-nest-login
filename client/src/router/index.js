b755edce5838d66d88c14ceb277fd7284c4f503f
Jefferson Rylee <rylee.jeff385@gmail.com>
2020-02-12 13:44:38 +0800

    Add home and login views Add RouterView

---
+++ client/src/router/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import * as View from "../views";
+
+export default function RouterView() {
+  return (
+    <Switch>
+      <Route path="/login">
+        <View.Login />
+      </Route>
+      <Route path="/">
+        <View.Home />
+      </Route>
+    </Switch>
+  );
+}
